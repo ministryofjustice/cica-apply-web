92bf5418f37cd706d3ed0ead05127f139ca9ba9d
Stephen Sinclair <Stephen.Sinclair@CICA.GOV.UK>
2019-02-04 16:10:46 +0000

    Setup govuk frontend

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+const nunjucks = require('nunjucks');
 
 // const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
+const indexRouter = require('./questionnaire/routes');
 
 const app = express();
+
+nunjucks.configure(
+    [
+        'node_modules/govuk-frontend/',
+        'node_modules/govuk-frontend/components/',
+        'questionnaire/',
+        'page/'
+    ],
+    {
+        autoescape: true,
+        express: app
+    }
+);
 
 app.use(helmet());
 app.use(logger('dev'));
@@ -16,7 +30,19 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/assets')));
+app.use(
+    '/govuk-frontend/all.css',
+    express.static(path.join(__dirname, '/public/stylesheets/all.css'))
+);
+app.use(
+    '/govuk-frontend/all-ie8.css',
+    express.static(path.join(__dirname, '/public/stylesheets/all-ie8.css'))
+);
+app.use(
+    '/govuk-frontend/all.js',
+    express.static(path.join(__dirname, '/node_modules/govuk-frontend/all.js'))
+);
+app.use('/', indexRouter);
 
 module.exports = app;
