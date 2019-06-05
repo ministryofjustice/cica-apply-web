04a91c44389ae79c74e40474ddc5ae85f0a3401a
BarryPiccinni <Barry.Piccinni@cica.gov.uk>
2019-06-05 11:32:47 +0100

    Revise conditional reveal validation
    This commit will add code which identifies answers to conditionally
    revealing questions which are not selected, and removes them. This
    keeps the answer object free of answers which we are not interested
    in, and resolves a bug where these additional answers were causing
    the schema to fail validation.
    
    This commit also updates the questionnaire to an up to date version.
    This update includes a fix to a bug which results in hidden fields
    throwing a required error despite their parent control not being
    used.
    
    This commit also includes an updated questionnaie to be compatible
    with the above changes.

--- questionnaire/routes.js
+++ questionnaire/routes.js
@@ -70,8 +70,29 @@ function logProgress(req, questionnaire) {
     console.log('-------------------\n\n\n');
 }
 
-function processRequest(reqBody) {
+function processRequest(reqBody, section) {
     const body = reqBody;
+    if (section in uiSchema && uiSchema[section].options.properties) {
+        Object.keys(uiSchema[section].options.properties).forEach(question => {
+            let convertedAnswer = body[question];
+            if (body[question] === 'true') {
+                convertedAnswer = true;
+            } else if (body[question] === 'false') {
+                convertedAnswer = false;
+            }
+            if (uiSchema[section].options.properties[question].options.conditionalComponentMap) {
+                uiSchema[section].options.properties[
+                    question
+                ].options.conditionalComponentMap.forEach(mapping => {
+                    if (convertedAnswer !== mapping.itemValue) {
+                        mapping.componentIds.forEach(id => {
+                            delete body[id];
+                        });
+                    }
+                });
+            }
+        });
+    }
 
     // Handle req.body
     Object.keys(body).forEach(property => {
@@ -194,7 +215,7 @@ router
                     : `/apply/previous/${removeSectionIdPrefix(sectionId)}`;
             const isSummary = sectionId === questionnaire.routes.summary;
 
-            processRequest(body);
+            processRequest(body, sectionId);
 
             const currentSchema = questionnaire.sections[sectionId];
             // Ajv mutates the data it's testing when using the coerce option
