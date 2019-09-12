3d5e900bb11320c2da71098123631a34f8a444ec
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2019-09-12 16:36:42 +0100

    remove middleware directory

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const logger = require('morgan');
 const helmet = require('helmet');
 const nunjucks = require('nunjucks');
 const clientSessions = require('client-sessions');
+const csrf = require('csurf');
 const formHelper = require('./questionnaire/form-helper');
 const qService = require('./questionnaire/questionnaire-service')();
 const indexRouter = require('./index/routes');
 const applicationRouter = require('./questionnaire/routes');
-const middleware = require('./middleware');
 
 const app = express();
 
@@ -50,7 +50,12 @@ app.use(
     })
 );
 
-app.use(middleware);
+const csrfProtection = csrf({
+    cookie: false,
+    sessionKey: 'cicaSession'
+});
+
+app.use(csrfProtection);
 
 // Suppression necessary as 'return' is needed to call res.end() end prevent the redirect throwing an error.
 // eslint-disable-next-line consistent-return
@@ -97,7 +102,7 @@ app.use('/apply', applicationRouter);
 
 app.use((err, req, res, next) => {
     if (err.name === 'EBADCSRFTOKEN') {
-        return res.status(403).render('404.njk');
+        return res.status(403).render('503.njk');
     }
 
     return next(err);
