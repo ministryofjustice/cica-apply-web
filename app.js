2e6cd47b450835cdd04d6146721ed1c5c3780009
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2019-09-19 14:17:48 +0100

    Update CSP

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.use(
         directives: {
             defaultSrc: ["'self'"],
             scriptSrc: ["'self'", "'unsafe-inline'"],
-            styleSrc: ["'self'", "'unsafe-inline'"],
             imgSrc: ["'self'", 'data:']
         }
     })
