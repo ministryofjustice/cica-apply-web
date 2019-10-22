31e002bfe8ca90b88f264890380400660afdce74
Stephen Sinclair <Stephen.Sinclair@CICA.GOV.UK>
2019-10-22 14:35:30 +0100

    Update kube deploy.yaml secrets for uat/prod

--- jest.setup.js
+++ jest.setup.js
@@ -2,7 +2,6 @@
 
 jest.setTimeout(30000);
 jest.testEnvironment = 'node';
-process.env.DATA_CAPTURE_SERVICE = 'some_token';
-process.env.DCS_JWT = 'A massive string';
+process.env.CW_DCS_JWT = 'A massive string';
 process.env.CW_COOKIE_SECRET = 'Also a huge string';
 process.env.CW_DCS_URL = 'http://docker.for.win.localhost:3100';
