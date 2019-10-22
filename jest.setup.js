14e335f528d32425275415f4ecc04d855c6bb91c
Stephen Sinclair <Stephen.Sinclair@CICA.GOV.UK>
2019-10-22 14:21:19 +0100

    Remove api version from CW_DCS_URL env var

--- jest.setup.js
+++ jest.setup.js
@@ -5,4 +5,4 @@ jest.testEnvironment = 'node';
 process.env.DATA_CAPTURE_SERVICE = 'some_token';
 process.env.DCS_JWT = 'A massive string';
 process.env.CW_COOKIE_SECRET = 'Also a huge string';
-process.env.CW_DCS_URL = 'http://docker.for.win.localhost:3100/api/v1';
+process.env.CW_DCS_URL = 'http://docker.for.win.localhost:3100';
