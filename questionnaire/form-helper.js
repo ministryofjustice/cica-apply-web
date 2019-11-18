bc5a785a57a1fadc355c621302b7131f8b2b3996
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2019-11-18 14:14:35 +0000

    Update deploy.yml files

--- questionnaire/form-helper.js
+++ questionnaire/form-helper.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const nunjucks = require('nunjucks');
-const qTransformer = require('../../q-transformer')();
+const qTransformer = require('q-transformer')();
 const uiSchema = require('./questionnaireUISchema');
 const sectionList = require('./non-complex-sexual-assault-id-mapper');
 
