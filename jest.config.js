a62f35f2114bb0c3bb69629870c8f010d51f82d6
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2020-01-28 11:42:41 +0000

    Add webpack + Babel

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 'use strict';
 
 /*! m0-start */
-module.exports = {
-    setupFilesAfterEnv: ['./jest.setup.js']
+const config = {
+    testEnvironment: 'node'
 };
+/*! m0-end */
+
+config.setupFilesAfterEnv = ['./jest.setup.js'];
+
+/*! m0-start */
+module.exports = config;
 /*! m0-end */
