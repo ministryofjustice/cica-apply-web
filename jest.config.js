fb8a79b10c0e4e657514adbb07224bcdb8b27ac8
sinclairs <stephen.sinclair@cica.gov.uk>
2020-03-27 09:53:56 +0000

    Sjs ci (#114)
    * Bump base config to v0.9.0
    
    * Add coverage threshold to jest config
    
    * Update config
    
    * Typo
    
    * Fix typo

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
-'use strict';
-
 /*! m0-start */
+
+'use strict';
 
 const config = {
     testEnvironment: 'node'
@@ -8,6 +8,21 @@ const config = {
 /*! m0-end */
 
 config.setupFilesAfterEnv = ['./jest.setup.js'];
+
+config.coverageThreshold = {
+    './**/!(autocomplete).js': {
+        branches: 60,
+        functions: 60,
+        lines: 60,
+        statements: 60
+    },
+    './src/modules/autocomplete/autocomplete.js': {
+        branches: 20,
+        functions: 20,
+        lines: 20,
+        statements: 20
+    }
+};
 
 /*! m0-start */
 module.exports = config;
