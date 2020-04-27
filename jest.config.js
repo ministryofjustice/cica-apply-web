36964fbe3c0293ea87d3cf614c4456f0e1f7ab47
sinclairs <stephen.sinclair@cica.gov.uk>
2020-04-27 13:06:30 +0100

    Add CI steps build, lint, test (#124)

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,21 @@ const config = {
 
 config.setupFilesAfterEnv = ['./jest.setup.js'];
 
+config.coverageThreshold = {
+    './**/!(autocomplete).js': {
+        branches: 60,
+        functions: 60,
+        lines: 60,
+        statements: 60
+    },
+    './src/modules/autocomplete/autocomplete.js': {
+        branches: 20,
+        functions: 20,
+        lines: 20,
+        statements: 20
+    }
+};
+
 /*! m0-start */
 module.exports = config;
 /*! m0-end */
