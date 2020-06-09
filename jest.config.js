7c939697c7d211c73569280a038e777e77b1dfb3
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2020-06-10 00:44:39 +0100

    Remove autocomplete workaround from Jest config

--- jest.config.js
+++ jest.config.js
@@ -10,17 +10,11 @@ const config = {
 config.setupFilesAfterEnv = ['./jest.setup.js'];
 
 config.coverageThreshold = {
-    './**/!(autocomplete).js': {
+    './**/**.js': {
         branches: 60,
         functions: 60,
         lines: 60,
         statements: 60
-    },
-    './src/modules/autocomplete/autocomplete.js': {
-        branches: 20,
-        functions: 20,
-        lines: 20,
-        statements: 20
     }
 };
 
