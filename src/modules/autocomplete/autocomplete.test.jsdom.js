f009569faa94ec69a2402032a6bcb5bdf3c2a167
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2020-06-10 20:32:51 +0100

    Tests made independant and config corrected

--- src/modules/autocomplete/autocomplete.test.jsdom.js
+++ src/modules/autocomplete/autocomplete.test.jsdom.js
@@ -1,9 +1,19 @@
-/* globals document */
 import {createAutocomplete} from './autocomplete';
 
 describe('autoComplete.js', () => {
     describe('autoComplete helper funtions', () => {
-        const autoComplete = createAutocomplete();
+        let autoComplete;
+        beforeEach(() => {
+            window.document.body.innerHTML = `<label for="dropdown-menu">Dropdown</label>
+                    <select id="dropdown-menu" class="govuk-select">
+                        <option value="">Item 1</option>
+                        <option value="2">Item 2</option>
+                        <option value="3">Item 3</option>
+                        <option value="4">Item 4</option>
+                        <option value="5">Item 5</option>
+                    </select>`;
+            autoComplete = createAutocomplete(window);
+        });
         describe('inputValueTemplate', () => {
             it('Should return the name given a valid result', () => {
                 const result = {name: 'result-name', value: 10001};
@@ -37,27 +47,12 @@ describe('autoComplete.js', () => {
         });
 
         describe('onConfirm', () => {
-            window.document.body.innerHTML = `
-                <div>
-                    <select id="dropdown-menu" class="govuk-select">
-                        <option value="">Default</option>
-                        <option value="2">Item 2</option>
-                        <option value="3">Item 3</option>
-                        <option value="4">Item 4</option>
-                        <option value="5">Item 5</option>
-                    </select>
-                </div>
-                `;
             it('Should set the dropdown to the result', () => {
-                // eslint-disable-next-line no-shadow
-                const autoComplete = createAutocomplete(window);
                 autoComplete.onConfirm({code: '2'});
                 expect(window.document.querySelector("[value='2']").selected).toEqual(true);
             });
 
             it('Should set the dropdown to the blank string value if there is no result the result', () => {
-                // eslint-disable-next-line no-shadow
-                const autoComplete = createAutocomplete(window);
                 autoComplete.onConfirm();
                 expect(window.document.querySelector("[value='']").selected).toEqual(true);
             });
@@ -65,15 +60,7 @@ describe('autoComplete.js', () => {
 
         describe('htmlCollectionToArray', () => {
             it('Should transform an array-like collection of HTMLElements into an array', () => {
-                document.body.innerHTML = `<label for="dropdown-menu">Dropdown</label>
-                    <select id="dropdown-menu" class="govuk-dropdown">
-                        <option value="1">Item 1</option>
-                        <option value="2">Item 2</option>
-                        <option value="3">Item 3</option>
-                        <option value="4">Item 4</option>
-                        <option value="5">Item 5</option>
-                    </select>`;
-                const selectElement = document.querySelector('#dropdown-menu');
+                const selectElement = window.document.querySelector('#dropdown-menu');
 
                 const actual = autoComplete.htmlCollectionToArray(selectElement);
                 expect(typeof actual).toBeDefined();
@@ -84,27 +71,18 @@ describe('autoComplete.js', () => {
 
         describe('formatResults', () => {
             it('Should transform an array of option elements to an array of result objects', () => {
-                document.body.innerHTML = `<label for="dropdown-menu">Dropdown</label>
-                    <select id="dropdown-menu" class="govuk-dropdown">
-                        <option value="101">Item 1</option>
-                        <option value="102">Item 2</option>
-                        <option value="103">Item 3</option>
-                        <option value="104">Item 4</option>
-                        <option value="105">Item 5</option>
-                    </select>`;
-
-                const selectElement = document.querySelector('#dropdown-menu');
+                const selectElement = window.document.querySelector('#dropdown-menu');
                 const resultArray = autoComplete.htmlCollectionToArray(selectElement);
 
                 const actual = autoComplete.formatResults(resultArray);
                 expect(typeof actual).toBeDefined();
                 expect(typeof actual).toEqual('object');
                 expect(actual.length).toEqual(5);
-                expect(actual).toContainEqual({code: '101', name: 'Item 1'});
-                expect(actual).toContainEqual({code: '102', name: 'Item 2'});
-                expect(actual).toContainEqual({code: '103', name: 'Item 3'});
-                expect(actual).toContainEqual({code: '104', name: 'Item 4'});
-                expect(actual).toContainEqual({code: '105', name: 'Item 5'});
+                expect(actual).toContainEqual({code: '', name: 'Item 1'});
+                expect(actual).toContainEqual({code: '2', name: 'Item 2'});
+                expect(actual).toContainEqual({code: '3', name: 'Item 3'});
+                expect(actual).toContainEqual({code: '4', name: 'Item 4'});
+                expect(actual).toContainEqual({code: '5', name: 'Item 5'});
             });
         });
     });
