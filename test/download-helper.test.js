78cf6089b5d30b549c88fc516bc3db6b6ecb99fc
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2022-07-01 09:39:47 +0100

    Add string replace to remove newline and carriage retrun from download helper test

--- test/download-helper.test.js
+++ test/download-helper.test.js
@@ -4,27 +4,8 @@ const moment = require('moment-timezone');
 const downloadHelper = require('../questionnaire/download-helper');
 
 const sectionData = require('./test-fixtures/downloads/check-your-answers-section');
+// const expectedSummaryHtml = 'gfhfhgfgfhffffhf';
 const expectedSummaryHtml = require('./test-fixtures/downloads/expected-summary-html');
-
-// Remove indentation from strings when comparing them
-// Remove indentation from strings when comparing them
-function removeIndentation(val) {
-    const regex = /^\s+/gm;
-
-    if (val && typeof val === 'object' && !Array.isArray(val)) {
-        return JSON.parse(
-            JSON.stringify(val, (key, value) => {
-                if (typeof value === 'string') {
-                    return value.replace(regex, '');
-                }
-
-                return value;
-            })
-        );
-    }
-
-    return val.replace(regex, '');
-}
 
 describe('download-helper functions', () => {
     describe('Should render Nunjucks', () => {
@@ -32,8 +13,8 @@ describe('download-helper functions', () => {
             const timestamp = moment('2022-01-01 17:15');
             const summaryHtml = downloadHelper.getSummaryHtml(sectionData, timestamp);
             // const transformationContent = {};
-            expect(removeIndentation(expectedSummaryHtml.html)).toEqual(
-                removeIndentation(summaryHtml)
+            expect(expectedSummaryHtml.html.replace(/[\n\r]/g, '')).toEqual(
+                summaryHtml.replace(/[\n\r]/g, '')
             );
         });
     });
