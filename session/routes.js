f9c8fc21f6f04f41fda4fc09ba486485936a82ce
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2022-09-01 16:30:32 +0100

    Add session resource routes

---
+++ session/routes.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const qService = require('../questionnaire/questionnaire-service')();
+const createCookieService = require('../cookie/cookie-service');
+
+const router = express.Router();
+
+router.route('/keep-alive').get(async (req, res) => {
+    try {
+        const response = await qService.keepAlive(req.session.questionnaireId);
+        const sessionResource = response.body?.data?.[0]?.attributes;
+        if (sessionResource) {
+            const cookieService = createCookieService({
+                req,
+                res,
+                cookieName: 'sessionExpiry'
+            });
+            cookieService.set('expires', sessionResource.expires);
+        }
+        res.json(response.body);
+    } catch (err) {
+        res.status(err.statusCode || 404).render('404.njk');
+    }
+});
+
+module.exports = router;
