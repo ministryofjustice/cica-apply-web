f2dec8a56773bd5087af42aecc076f303f352c72
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2022-11-15 13:43:08 +0000

    Add address finder service

---
+++ address-finder/address-finder-service.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const service = require('../questionnaire/request-service')();
+
+const addressFinderEndpoint = 'https://api.os.uk/search/places/v1/postcode';
+const apikey = 'ADD_TO_SECRETS';
+
+// TODO should this be async?
+function addressFinderService() {
+    function lookupPostcode(postcode) {
+        const opts = {
+            url: `${addressFinderEndpoint}?postcode=${postcode}`,
+            headers: {
+                Key: apikey
+            }
+        };
+        return service.get(opts);
+    }
+    return Object.freeze({
+        lookupPostcode
+    });
+}
+
+module.exports = addressFinderService;
