fdb3b8511ac98db1956b208035cb051b17671c1a
BarryPiccinni <Barry.Piccinni@cica.gov.uk>
2022-11-16 15:21:19 +0000

    Add /account/signed-in route (#284)

--- govuk/sign-in/index.test.js
+++ govuk/sign-in/index.test.js
@@ -1,26 +1,48 @@
 'use strict';
 
 describe('Sign In service', () => {
-    beforeAll(() => {
-        jest.resetModules();
-        jest.doMock('./issuer/index', () =>
-            jest.fn(() => ({
-                identify: () => 'issuer identified'
-            }))
-        );
-        jest.doMock('./authorisation/index', () =>
-            jest.fn(() => ({
-                getAuthorisationURI: () => 'A_VALID_URL'
-            }))
-        );
+    describe('Get Service Url', () => {
+        beforeAll(() => {
+            jest.resetModules();
+            jest.doMock('./issuer/index', () =>
+                jest.fn(() => ({
+                    identify: () => 'issuer identified'
+                }))
+            );
+            jest.doMock('./authorisation/index', () =>
+                jest.fn(() => ({
+                    getAuthorisationURI: () => 'A_VALID_URL'
+                }))
+            );
+        });
+
+        it('Should identify the govuk service and construct the url necessary to sign in', async () => {
+            // eslint-disable-next-line global-require
+            const createSignInService = require('./index');
+            const signInService = createSignInService();
+            const actual = await signInService.getServiceUrl();
+
+            expect(actual).toEqual('A_VALID_URL');
+        });
     });
 
-    it('Should identify the govuk service and construct the url necessary to sign in', async () => {
-        // eslint-disable-next-line global-require
-        const createSignInService = require('./index');
-        const signInService = createSignInService();
-        const actual = await signInService.getServiceUrl();
+    describe('Get UserId Token', () => {
+        beforeAll(() => {
+            jest.resetModules();
+            jest.doMock('./token/index', () =>
+                jest.fn(() => ({
+                    getUserIdToken: () => 'A Token'
+                }))
+            );
+        });
 
-        expect(actual).toEqual('A_VALID_URL');
+        it('Should return the userId token', async () => {
+            // eslint-disable-next-line global-require
+            const createSignInService = require('./index');
+            const signInService = createSignInService();
+            const actual = await signInService.getUserIdToken({});
+
+            expect(actual).toEqual('A Token');
+        });
     });
 });
