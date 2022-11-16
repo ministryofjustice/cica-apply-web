a43cbf6c2fdcbe78e7d6e72e91d09826e5ba34bb
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2022-11-16 17:43:06 +0000

    Refactor address finder tests

--- address-finder/routes.js
+++ address-finder/routes.js
@@ -10,8 +10,8 @@ router.route('/postcode').get(async (req, res, next) => {
     try {
         const {postcode} = req.query;
         const response = await addressFinder.lookupPostcode(postcode);
-
-        return res.json(response);
+        // this works from the front end
+        return res.status(response.statusCode).send(response.body);
     } catch (err) {
         res.status(err.statusCode || 404).render('404.njk');
         return next(err);
