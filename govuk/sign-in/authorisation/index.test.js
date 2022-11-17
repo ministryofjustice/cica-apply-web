0987fdf9d8a40706d7e2b3e106d905582853eaf5
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2022-11-17 16:55:37 +0000

    Add govuk state handling and verification

--- govuk/sign-in/authorisation/index.test.js
+++ govuk/sign-in/authorisation/index.test.js
@@ -12,7 +12,7 @@ describe('authorisation service', () => {
         };
         const redirectUri = 'http://www.redirect.com';
         const expected =
-            'http://www.this_is_a_host.com/?scope=openid&response_type=code&client_id=RSsuV9e2KzU9IgtvOJsyZIOVy8U&redirect_uri=http%3A%2F%2Fwww.redirect.com&state=STATE&nonce=NONCE&vtr=%5BCl.Cm%5D';
+            'http://www.this_is_a_host.com/?scope=openid&response_type=code&client_id=RSsuV9e2KzU9IgtvOJsyZIOVy8U&redirect_uri=https%3A%2F%2Fwww.gov.uk%2F&state=STATE&nonce=NONCE&vtr=%5BCl.Cm%5D';
 
         const actual = authorisationService.getAuthorisationURI({issuer, redirectUri});
 
@@ -30,15 +30,15 @@ describe('authorisation service', () => {
             scope: 'closedid',
             response_type: 'chaos',
             client_id: 'iAmGuessable',
+            redirect_uri: 'http://www.redirect.com',
             state: 'STATELESS',
             nonce: 'ECNON',
             vtr: '[untrusted]'
         };
-        const redirectUri = 'http://www.redirect.com';
         const expected =
             'http://www.this_is_a_host.com/?scope=closedid&response_type=chaos&client_id=iAmGuessable&redirect_uri=http%3A%2F%2Fwww.redirect.com&state=STATELESS&nonce=ECNON&vtr=%5Buntrusted%5D';
 
-        const actual = authorisationService.getAuthorisationURI({issuer, redirectUri, options});
+        const actual = authorisationService.getAuthorisationURI({issuer, options});
 
         expect(actual).toEqual(expected);
     });
