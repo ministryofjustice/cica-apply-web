823b52ec1bbf4c67c5ff66258b368524d65dcf5f
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2022-11-17 10:07:40 +0000

    Add jsdom address search api tests

--- address-finder/test/address-finder-service.test.js
+++ address-finder/test/address-finder-service.test.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const getAddressCollectionResponse = require('./fixtures/validAddressCollectionResponse.json');
+const noAddressesFoundResponse = require('./fixtures/noAddressesFoundResponse.json');
+
 // see https://apidocs.os.uk/reference/os-places-postcode
-describe('Address finder service', () => {
-    describe('find Address by Postcode lookup', () => {
+describe('find Address by Postcode lookup', () => {
+    describe('200', () => {
         it('Should respond with status code 200 and return an Adress collection response given a valid postcode', async () => {
             jest.doMock('../../questionnaire/request-service', () =>
                 jest.fn(() => ({
@@ -16,6 +18,22 @@ describe('Address finder service', () => {
             const response = await addressFinderService.lookupPostcode('CF645UL');
             expect(response).toEqual(getAddressCollectionResponse);
         });
+
+        it('Should respond with status code 200 and return the correct header response when no addresses found', async () => {
+            jest.doMock('../../questionnaire/request-service', () =>
+                jest.fn(() => ({
+                    get: () => noAddressesFoundResponse
+                }))
+            );
+            jest.resetModules();
+            // eslint-disable-next-line global-require
+            const addressFinderService = require('../address-finder-service')();
+            const response = await addressFinderService.lookupPostcode('AB125CD');
+            expect(response).toEqual(noAddressesFoundResponse);
+        });
+    });
+
+    describe('400', () => {
         it('Should respond with status code 400 and return an error response when no postcode parameter provided', async () => {
             const noPostcodeProvidedError = {
                 statusCode: 400,
@@ -62,6 +80,33 @@ describe('Address finder service', () => {
             const response = await addressFinderService.lookupPostcode();
             expect(response).toEqual(invalidPostcodeProvidedError);
         });
+
+        it('Should respond with status code 400 and return an error response when postcode parameter does not contain minimum of sector plus 1 digit', async () => {
+            const invalidPostcodeProvidedError = {
+                statusCode: 400,
+                body: {
+                    error: {
+                        statuscode: 400,
+                        message:
+                            'Requested postcode must contain a minimum of the sector plus 1 digit of the district e.g. SO1. Requested postcode was AB12CD'
+                    }
+                }
+            };
+
+            jest.doMock('../../questionnaire/request-service', () =>
+                jest.fn(() => ({
+                    get: () => invalidPostcodeProvidedError
+                }))
+            );
+            jest.resetModules();
+            // eslint-disable-next-line global-require
+            const addressFinderService = require('../address-finder-service')();
+            const response = await addressFinderService.lookupPostcode();
+            expect(response).toEqual(invalidPostcodeProvidedError);
+        });
+    });
+
+    describe('401', () => {
         it('Should respond with status code 401 and return an error response for invalid api key', async () => {
             const apiKeyInvalidFault = {
                 statusCode: 401,
@@ -86,7 +131,8 @@ describe('Address finder service', () => {
             const response = await addressFinderService.lookupPostcode('CF645UL');
             expect(response).toEqual(apiKeyInvalidFault);
         });
-
+    });
+    describe('500', () => {
         it('Should respond with status code 500 and return an error response for internal service error', async () => {
             const internalServiceError = {
                 statusCode: 500,
