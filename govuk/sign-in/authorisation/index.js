e629e3ed87c31a791d50f25b408b4c5602f0fc39
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2022-11-17 16:55:37 +0000

    Add govuk state handling and verification

--- govuk/sign-in/authorisation/index.js
+++ govuk/sign-in/authorisation/index.js
@@ -5,22 +5,14 @@ function createAuthorisationService() {
         scope: 'openid',
         response_type: 'code',
         client_id: 'RSsuV9e2KzU9IgtvOJsyZIOVy8U',
+        redirect_uri: 'https://www.gov.uk/',
         state: 'STATE',
         nonce: 'NONCE',
         vtr: '[Cl.Cm]'
     };
-    function getAuthorisationURI({issuer, redirectUri, options = defaults}) {
-        const opts = {
-            scope: options.scope,
-            response_type: options.response_type,
-            client_id: options.client_id,
-            redirect_uri: redirectUri,
-            state: options.state,
-            nonce: options.nonce,
-            vtr: options.vtr
-        };
+    function getAuthorisationURI({issuer, options}) {
+        const opts = {...defaults, ...options};
         const searchParams = new URLSearchParams(opts);
-
         return `${issuer.metadata.authorization_endpoint}?${searchParams.toString()}`;
     }
 
