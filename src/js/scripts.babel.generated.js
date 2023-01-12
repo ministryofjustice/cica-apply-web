88755c23be41c0c593dbf528ecb2e60944a5ee51
BarryPiccinni <Barry.Piccinni@cica.gov.uk>
2023-01-12 12:06:14 +0000

    Add user dashboard (#313)

--- src/js/scripts.babel.generated.js
+++ src/js/scripts.babel.generated.js
@@ -364,6 +364,8 @@ var axios = _interopRequireWildcard(require("axios"));
 
 var jsCookies = _interopRequireWildcard(require("js-cookie"));
 
+var _frontend = require("@ministryofjustice/frontend");
+
 var _ga = _interopRequireDefault(require("../modules/ga"));
 
 var _autocomplete = _interopRequireDefault(require("../modules/autocomplete/autocomplete"));
@@ -605,4 +607,5 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
   (0, _newWindowAnchors.default)(window.document.querySelectorAll('[open-new-window]'));
   (0, _liveChat.default)(window.document.querySelector('#chat-iframe'));
+  (0, _frontend.initAll)();
 })();
