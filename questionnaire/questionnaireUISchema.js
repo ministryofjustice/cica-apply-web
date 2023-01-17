8aab3780286f5bfb742fc13a363ea24c7f551c5b
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2023-01-17 18:32:25 +0000

    Fix dentist organisation name not displaying on error summary

--- questionnaire/questionnaireUISchema.js
+++ questionnaire/questionnaireUISchema.js
@@ -431,6 +431,7 @@ module.exports = {
                 }
             },
             outputOrder: [
+                'q-gp-organisation-name',
                 'q-gp-building-and-street',
                 'q-gp-building-and-street-2',
                 'q-gp-building-and-street-3',
