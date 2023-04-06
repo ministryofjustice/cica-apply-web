99a0970dc30a4ea9c77f524b7d94f7a6087a051e
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2023-04-06 12:26:55 +0100

    Add `oidc` session implementation test coverage

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,7 @@
 
 jest.setTimeout(3000);
 jest.testEnvironment = 'node';
+process.env.CW_URL = 'http://cw.com';
 process.env.CW_DCS_JWT = 'A massive string';
 process.env.CW_COOKIE_SECRET = 'Also a huge string';
 process.env.CW_DCS_URL = 'http://docker.for.win.localhost:3100';
@@ -13,3 +14,7 @@ process.env.CW_LIVECHAT_START_TIMES =
     '00:00:01.000,00:00:01.000,00:00:01.000,00:00:01.000,00:00:01.000,00:00:01.000,00:00:01.000';
 process.env.CW_LIVECHAT_END_TIMES =
     '23:59:59.000,23:59:59.000,23:59:59.000,23:59:59.000,23:59:59.000,23:59:59.000,23:59:59.000';
+process.env.CW_GOVUK_CLIENT_ID = 'thisistheclientid';
+process.env.CW_GOVUK_PRIVATE_KEY = 'thisisthegovukprivatekey';
+process.env.CW_GOVUK_ISSUER_URL = 'http://www.issuer.com';
+process.env.CW_SESSION_DURATION = 600000;
