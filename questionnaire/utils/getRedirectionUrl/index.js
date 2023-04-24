66a55ac76dedb1ca6a0fd25526c92d6ffdcc8aa2
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2023-04-24 14:31:13 +0100

    Add ability to resume a questionnaire

--- questionnaire/utils/getRedirectionUrl/index.js
+++ questionnaire/utils/getRedirectionUrl/index.js
@@ -1,11 +1,19 @@
 'use strict';
 
-const urls = {
-    start: '/apply/start'
-};
+function getRedirectionUrl(type, questionnaireId) {
+    const inProgresstatus = questionnaireId ? 'started' : 'notstarted';
+    const urls = {
+        notstarted: {
+            start: '/apply/start',
+            resume: '/account/dashboard'
+        },
+        started: {
+            start: '/apply/start',
+            resume: `/apply/resume/${questionnaireId}`
+        }
+    };
 
-function getRedirectionUrl(type) {
-    const redirectionUrl = urls[type];
+    const redirectionUrl = urls[inProgresstatus][type];
     return redirectionUrl;
 }
 
