95e0bc4bb763e23438ef1b1a0f9a2198dfe5b9f8
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2023-04-24 14:31:13 +0100

    Add ability to resume a questionnaire

--- questionnaire/utils/getRedirectionUrl/getRedirectionUrl.test.js
+++ questionnaire/utils/getRedirectionUrl/getRedirectionUrl.test.js
@@ -3,14 +3,60 @@
 const getRedirectionUrl = require('.');
 
 describe('getRedirectionUrl', () => {
-    it('Should return a valid URL', () => {
-        const type = 'start';
-        const result = getRedirectionUrl(type);
-        expect(result).toBe('/apply/start');
+    describe('No questionnaireId supplied', () => {
+        describe('Start type equals "start"', () => {
+            it('Should return a valid start URL', () => {
+                const type = 'start';
+                const questionnaireId = undefined;
+                const result = getRedirectionUrl(type, questionnaireId);
+                expect(result).toBe('/apply/start');
+            });
+        });
+
+        describe('Start type equals "resume"', () => {
+            it('Should return a valid resume URL', () => {
+                const type = 'resume';
+                const questionnaireId = undefined;
+                const result = getRedirectionUrl(type, questionnaireId);
+                expect(result).toBe('/account/dashboard');
+            });
+        });
+
+        describe('Start type equals "invalid"', () => {
+            it('Should return undefined', () => {
+                const type = 'invalid';
+                const questionnaireId = undefined;
+                const result = getRedirectionUrl(type, questionnaireId);
+                expect(result).toBe(undefined);
+            });
+        });
     });
-    it('Should return undefined', () => {
-        const type = 'notavalidtype';
-        const result = getRedirectionUrl(type);
-        expect(result).toBe(undefined);
+    describe('QuestionnaireId supplied', () => {
+        describe('Start type equals "start"', () => {
+            it('Should return a valid start URL', () => {
+                const type = 'start';
+                const questionnaireId = 'some-questionnaire-id';
+                const result = getRedirectionUrl(type, questionnaireId);
+                expect(result).toBe('/apply/start');
+            });
+        });
+
+        describe('Start type equals "resume"', () => {
+            it('Should return a valid resume URL', () => {
+                const type = 'resume';
+                const questionnaireId = 'some-questionnaire-id';
+                const result = getRedirectionUrl(type, questionnaireId);
+                expect(result).toBe('/apply/resume/some-questionnaire-id');
+            });
+        });
+
+        describe('Start type equals "invalid"', () => {
+            it('Should return undefined', () => {
+                const type = 'invalid';
+                const questionnaireId = 'some-questionnaire-id';
+                const result = getRedirectionUrl(type, questionnaireId);
+                expect(result).toBe(undefined);
+            });
+        });
     });
 });
