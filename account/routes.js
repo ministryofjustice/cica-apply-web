279bb6e16f8fa025d58b5e3899a0ed5c2aab5112
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2023-06-19 11:22:37 +0100

    Add dashbaord functionality

--- account/routes.js
+++ account/routes.js
@@ -8,31 +8,43 @@ const {getDashboardURI} = require('./utils/getActionURIs');
 const createAccountService = require('./account-service');
 const createQuestionnaireService = require('../questionnaire/questionnaire-service');
 const getQuestionnaireIdInSession = require('../questionnaire/utils/getQuestionnaireIdInSession');
+const createDashboardService = require('../dashboard/dashboard-service');
 
 const router = express.Router();
 
 router.get('/sign-in/success', requiresAuth(), async (req, res, next) => {
     try {
-        const expiryDate = new Date(
-            new Date().setDate(new Date().getDate() + 31)
-        ).toLocaleDateString('en-GB', {year: 'numeric', month: 'long', day: 'numeric'});
-
         const templateEngineService = createTemplateEngineService();
         const accountService = createAccountService(req.session);
         const {render} = templateEngineService;
 
         const questionnaireId = getQuestionnaireIdInSession(req.session);
+        const questionnaireServiceUnauthenticated = createQuestionnaireService({
+            ownerId: accountService.getOwnerId()
+        });
         if (questionnaireId) {
-            const questionnaireService = createQuestionnaireService({
-                id: accountService.getOwnerId(),
-                isAuthenticated: true
-            });
-            await questionnaireService.postSection(questionnaireId, 'owner', {
-                id: req.oidc.user.sub,
-                isAuthenticated: true
+            await questionnaireServiceUnauthenticated.postSection(questionnaireId, 'owner', {
+                'owner-id': req.oidc.user.sub,
+                'is-authenticated': true
             });
         }
         accountService.setOwnerId(req.oidc.user.sub);
+
+        const questionnaireServiceAuthenticated = createQuestionnaireService({
+            ownerId: req.oidc.user.sub
+        });
+        const questionnaireMetadata = await questionnaireServiceAuthenticated.getQuestionnaireMetadata(
+            questionnaireId
+        );
+
+        const expiryDate = new Date(
+            questionnaireMetadata.body.data[0].attributes.expires
+        ).toLocaleDateString('en-GB', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            timeZone: 'Europe/London'
+        });
 
         const html = render('authentication-success.njk', {
             nextPageUrl: getValidReferrerOrDefault(req?.session?.referrer),
@@ -77,9 +89,16 @@ router.get('/dashboard', requiresAuth(), async (req, res, next) => {
     try {
         const templateEngineService = createTemplateEngineService();
         const accountService = createAccountService(req.session);
+        const ownerId = req.oidc.user.sub;
+        accountService.setOwnerId(ownerId);
+
+        const dashboardService = createDashboardService(ownerId);
+        const ownerData = await dashboardService.getTemplateData();
+
         const {render} = templateEngineService;
         const html = render('dashboard.njk', {
-            isAuthenticated: accountService.isAuthenticated(req)
+            isAuthenticated: accountService.isAuthenticated(req),
+            ownerData
         });
         return res.send(html);
     } catch (err) {
---
+++ dashboard/dashboard-service.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const createQuestionnaireService = require('../questionnaire/questionnaire-service');
+
+function createDashboardService(ownerId) {
+    const questionnaireService = createQuestionnaireService({ownerId});
+    async function getFlatQuestionnaireMetadataByUserId() {
+        const metadataCollection = await questionnaireService.getAllQuestionnairesMetadata();
+        return (metadataCollection.body.data || []).map(metadatum => {
+            return {
+                questionnaireId: metadatum.attributes['questionnaire-id'],
+                expires: metadatum.attributes.expires
+            };
+        });
+    }
+
+    async function getTemplateData() {
+        const dataset = [];
+        // eslint-disable-next-line no-restricted-syntax
+        for await (const flatMetadata of await getFlatQuestionnaireMetadataByUserId()) {
+            const answers = await questionnaireService.getSectionAnswers(
+                flatMetadata.questionnaireId,
+                'p-applicant-enter-your-name'
+            );
+            dataset.push({
+                ...flatMetadata,
+                answers: {
+                    'q-applicant-title': '',
+                    'q-applicant-first-name': '',
+                    'q-applicant-last-name': '',
+                    ...answers.body?.data?.attributes
+                }
+            });
+        }
+        const templateData = dataset.reduce((acc, questionnaireData) => {
+            acc.push([
+                {
+                    text: `${questionnaireData.answers['q-applicant-first-name']} ${questionnaireData.answers['q-applicant-last-name']}`,
+                    classes: 'govuk-table__cell__overflow'
+                },
+                {
+                    text: new Date(questionnaireData.expires).toLocaleDateString('en-GB', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                        timeZone: 'Europe/London'
+                    }),
+                    attributes: {
+                        'data-sort-value': new Date(questionnaireData.expires).getTime()
+                    }
+                },
+                {
+                    html: `<a href="/apply/resume/${questionnaireData.questionnaireId}" class="govuk-link">Continue<span class='govuk-visually-hidden'> Continue application for ${questionnaireData.answers['q-applicant-first-name']} ${questionnaireData.answers['q-applicant-last-name']}</span></a>`
+                }
+            ]);
+            return acc;
+        }, []);
+
+        return templateData;
+    }
+
+    return Object.freeze({
+        getTemplateData
+    });
+}
+
+module.exports = createDashboardService;
