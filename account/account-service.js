fc87fbdb29d2c0a416cd2a23a512b7ea6ae14c84
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2023-06-30 15:47:32 +0100

    Add account module

---
+++ account/account-service.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const crypto = require('crypto');
+
+function createAccountService(session) {
+    const currentSession = session;
+
+    function URN_UUID() {
+        return `urn:uuid:${crypto.randomUUID()}`;
+    }
+
+    function generateOwnerId() {
+        const ownerId = URN_UUID();
+        currentSession.ownerId = ownerId;
+        return ownerId;
+    }
+
+    function getOwnerId() {
+        if (!currentSession?.ownerId) {
+            return generateOwnerId();
+        }
+        return currentSession?.ownerId;
+    }
+
+    function setOwnerId(id) {
+        currentSession.ownerId = id;
+    }
+
+    function isAuthenticated(req) {
+        return !!(req?.oidc?.isAuthenticated() || req?.isAuthenticated);
+    }
+
+    return Object.freeze({
+        getOwnerId,
+        setOwnerId,
+        isAuthenticated
+    });
+}
+
+module.exports = createAccountService;
