0b2cf5c61b553baf56d60e0f196872895fb38c2f
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2023-07-05 15:34:46 +0100

    Add new `/apply`, `/start`, `/resume` routes

--- questionnaire/routes.js
+++ questionnaire/routes.js
@@ -2,27 +2,142 @@
 
 const express = require('express');
 const formHelper = require('./form-helper');
-const qService = require('./questionnaire-service')();
+const createQuestionnaireService = require('./questionnaire-service');
+const getFormSubmitButtonText = require('./utils/getFormSubmitButtonText');
+const getQuestionnaireIdInSession = require('./utils/getQuestionnaireIdInSession');
+const createAccountService = require('../account/account-service');
+const getRedirectionUrl = require('./utils/getRedirectionUrl');
+const createTemplateEngineService = require('../templateEngine');
 
 const router = express.Router();
 
-router.route('/').get(async (req, res) => {
+router.get('/', (req, res) => {
     try {
-        const response = await qService.getFirstSection(req.session.questionnaireId);
-        const responseBody = response.body;
-        const initialSection = formHelper.removeSectionIdPrefix(
-            responseBody.data[0].attributes.sectionId
+        const questionnaireId = getQuestionnaireIdInSession(req.session);
+        if (questionnaireId) {
+            return res.redirect(`/apply/resume/${questionnaireId}`);
+        }
+        return res.redirect('/apply/start-or-resume');
+    } catch (err) {
+        return res.status(err.statusCode || 404).render('404.njk');
+    }
+});
+
+router.route('/start-or-resume').get((req, res) => {
+    try {
+        const templateEngineService = createTemplateEngineService();
+        const accountService = createAccountService(req.session);
+        const {render} = templateEngineService;
+        const html = render('start-or-resume.njk', {
+            submitButtonText: getFormSubmitButtonText('start'),
+            csrfToken: req.csrfToken(),
+            isAuthenticated: accountService.isAuthenticated(req)
+        });
+        return res.send(html);
+    } catch (err) {
+        return res.status(err.statusCode || 404).render('404.njk');
+    }
+});
+
+router.post('/start-or-resume', (req, res) => {
+    try {
+        const templateEngineService = createTemplateEngineService();
+        const accountService = createAccountService(req.session);
+        const {render} = templateEngineService;
+        const startType = req.body['start-or-resume'];
+        const redirectionUrl = getRedirectionUrl(
+            startType,
+            getQuestionnaireIdInSession(req.session)
         );
-        res.redirect(`${req.baseUrl}/${initialSection}`);
+
+        if (redirectionUrl) {
+            return res.redirect(redirectionUrl);
+        }
+
+        const html = render('start-or-resume.njk', {
+            submitButtonText: getFormSubmitButtonText('start'),
+            csrfToken: req.csrfToken(),
+            error: {
+                text: 'Select what you would like to do'
+            },
+            isAuthenticated: accountService.isAuthenticated(req)
+        });
+        return res.send(html);
+    } catch (err) {
+        return res.status(err.statusCode || 404).render('404.njk');
+    }
+});
+
+router.route('/start').get(async (req, res) => {
+    try {
+        const accountService = createAccountService(req.session);
+        const questionnaireService = createQuestionnaireService({
+            ownerId: accountService.getOwnerId(),
+            isAuthenticated: accountService.isAuthenticated(req)
+        });
+        const response = await questionnaireService.createQuestionnaire();
+        const initialSection = formHelper.removeSectionIdPrefix(
+            response.body.data.attributes.routes.initial
+        );
+        req.session.questionnaireId = response.body.data.attributes.id;
+
+        res.redirect(`/apply/${initialSection}`);
     } catch (err) {
         res.status(err.statusCode || 404).render('404.njk');
     }
 });
 
+router.get('/resume/:questionnaireId', async (req, res) => {
+    try {
+        const accountService = createAccountService(req.session);
+        const questionnaireService = createQuestionnaireService({
+            ownerId: accountService.getOwnerId(),
+            isAuthenticated: accountService.isAuthenticated(req)
+        });
+
+        const defaultRedirect = '/apply';
+        let redirectUrl = defaultRedirect;
+
+        const resumableQuestionnaireId = req.params.questionnaireId;
+        const resumableQuestionnaireResponse = await questionnaireService.getCurrentSection(
+            resumableQuestionnaireId
+        );
+
+        if (resumableQuestionnaireResponse.body?.errors) {
+            const errorResponse = resumableQuestionnaireResponse.body?.errors[0];
+            if (errorResponse.status === 404) {
+                return res.redirect(redirectUrl);
+            }
+        }
+
+        const resumableQuestionnaireCurrentSectionId =
+            resumableQuestionnaireResponse.body?.data?.[0]?.relationships?.section?.data?.id;
+
+        // switch session info and redirect if valid.
+        if (resumableQuestionnaireCurrentSectionId) {
+            req.session.questionnaireId = resumableQuestionnaireId;
+            redirectUrl = `${redirectUrl}/${formHelper.removeSectionIdPrefix(
+                resumableQuestionnaireCurrentSectionId
+            )}`;
+        }
+        return res.redirect(redirectUrl);
+    } catch (err) {
+        return res.status(err.statusCode || 404).render('404.njk');
+    }
+});
+
 router.route('/previous/:section').get(async (req, res) => {
     try {
+        const accountService = createAccountService(req.session);
+        const questionnaireService = createQuestionnaireService({
+            ownerId: accountService.getOwnerId(),
+            isAuthenticated: accountService.isAuthenticated(req)
+        });
         const sectionId = formHelper.addPrefix(req.params.section);
-        const response = await qService.getPrevious(req.session.questionnaireId, sectionId);
+        const response = await questionnaireService.getPrevious(
+            req.session.questionnaireId,
+            sectionId
+        );
         if (response.body.data[0].attributes && response.body.data[0].attributes.url !== null) {
             const overwriteId = response.body.data[0].attributes.url;
             return res.redirect(overwriteId);
@@ -40,32 +155,28 @@ router
     .route('/:section')
     .get(async (req, res, next) => {
         try {
+            const accountService = createAccountService(req.session);
+            const isAuthenticated = accountService.isAuthenticated(req);
+            const questionnaireService = createQuestionnaireService({
+                ownerId: accountService.getOwnerId(),
+                isAuthenticated
+            });
             const sectionId = formHelper.addPrefix(req.params.section);
-            const response = await qService.getSection(req.session.questionnaireId, sectionId);
-            if (
-                response.body.data &&
-                response.body.data[0].attributes &&
-                response.body.data[0].attributes.sectionId
-            ) {
-                const isSummaryPage =
-                    formHelper.getSectionContext(response.body.data[0].attributes.sectionId) ===
-                    'summary';
+            const response = await questionnaireService.getSection(
+                req.session.questionnaireId,
+                sectionId
+            );
 
-                if (isSummaryPage) {
-                    res.set({
-                        'Cache-Control': 'private, no-cache, no-store, must-revalidate',
-                        Expires: '-1',
-                        Pragma: 'no-cache'
-                    });
-                }
-            }
+            req.session.referrer = req.originalUrl;
+
             const html = formHelper.getSectionHtml(
                 response.body,
                 req.csrfToken(),
-                res.locals.nonce
+                res.locals.nonce,
+                isAuthenticated
             );
             if (formHelper.getSectionContext(sectionId) === 'confirmation') {
-                req.session.reset();
+                delete req.session.questionnaireId;
             }
             res.send(html);
         } catch (err) {
@@ -75,25 +186,32 @@ router
     })
     .post(async (req, res, next) => {
         try {
+            const accountService = createAccountService(req.session);
+            const isAuthenticated = accountService.isAuthenticated(req);
+            const questionnaireService = createQuestionnaireService({
+                ownerId: accountService.getOwnerId(),
+                isAuthenticated
+            });
             const sectionId = formHelper.addPrefix(req.params.section);
             const body = formHelper.processRequest(req.body, req.params.section);
             let nextSectionId;
             // delete the token from the body to allow AJV to validate the request.
             // eslint-disable-next-line no-underscore-dangle
             delete body._csrf;
-            const response = await qService.postSection(
-                req.session.questionnaireId,
+            const response = await questionnaireService.postSection(
+                getQuestionnaireIdInSession(req.session),
                 sectionId,
                 body
             );
+
             if (response.statusCode === 201) {
                 // if the page is a submission
                 const isApplicationSubmission =
                     formHelper.getSectionContext(sectionId) === 'submission';
                 if (isApplicationSubmission) {
                     try {
-                        await qService.postSubmission(req.session.questionnaireId);
-                        const submissionResponse = await qService.getSubmissionStatus(
+                        await questionnaireService.postSubmission(req.session.questionnaireId);
+                        const submissionResponse = await questionnaireService.getSubmissionStatus(
                             req.session.questionnaireId,
                             Date.now()
                         );
@@ -111,8 +229,8 @@ router
                 }
 
                 if ('next' in req.query) {
-                    const progressEntryResponse = await qService.getSection(
-                        req.session.questionnaireId,
+                    const progressEntryResponse = await questionnaireService.getSection(
+                        getQuestionnaireIdInSession(req.session),
                         formHelper.addPrefix(req.query.next)
                     );
 
@@ -124,8 +242,8 @@ router
                     }
                 }
 
-                const progressEntryResponse = await qService.getCurrentSection(
-                    req.session.questionnaireId
+                const progressEntryResponse = await questionnaireService.getCurrentSection(
+                    getQuestionnaireIdInSession(req.session)
                 );
                 nextSectionId = formHelper.removeSectionIdPrefix(
                     progressEntryResponse.body.data[0].attributes.sectionId
@@ -138,7 +256,8 @@ router
                 response.body,
                 sectionId,
                 req.csrfToken(),
-                res.locals.nonce
+                res.locals.nonce,
+                isAuthenticated
             );
             return res.send(html);
         } catch (err) {
