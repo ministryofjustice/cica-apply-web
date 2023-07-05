c5367d536a9a0d678306b4e76b8f33db9dddcaff
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2023-07-05 15:19:25 +0100

    Add OIDC middleware + use `/account` router

--- app.js
+++ app.js
@@ -5,25 +5,26 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
-const clientSessions = require('client-sessions');
 const csrf = require('csurf');
 const {nanoid} = require('nanoid');
-const formHelper = require('./questionnaire/form-helper');
-const qService = require('./questionnaire/questionnaire-service')();
+const {auth} = require('express-openid-connect');
+const createQuestionnaireService = require('./questionnaire/questionnaire-service');
 const indexRouter = require('./index/routes');
 const applicationRouter = require('./questionnaire/routes');
 const downloadRouter = require('./download/routes');
 const sessionRouter = require('./session/routes');
 const addressFinderRouter = require('./address-finder/routes');
+const accountRouter = require('./account/routes');
 const createCookieService = require('./cookie/cookie-service');
 const createTemplateEngineService = require('./templateEngine');
+const isQuestionnaireInstantiated = require('./questionnaire/utils/isQuestionnaireInstantiated');
+const getQuestionnaireIdInSession = require('./questionnaire/utils/getQuestionnaireIdInSession');
+const createAccountService = require('./account/account-service');
 
 const app = express();
 
 const templateEngineService = createTemplateEngineService(app);
 templateEngineService.init();
-
-const sessionDuration = Number.parseInt(process.env.CW_SESSION_DURATION, 10);
 
 app.use((req, res, next) => {
     res.locals.nonce = nanoid();
@@ -68,29 +69,6 @@ app.use(
     })
 );
 
-app.use(
-    clientSessions({
-        cookieName: 'session', // cookie name dictates the key name added to the request object
-        secret: process.env.CW_COOKIE_SECRET, // should be a large unguessable string
-        duration: sessionDuration, // how long the session will stay valid in ms
-        activeDuration: sessionDuration, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
-        cookie: {
-            ephemeral: true, // when true, cookie expires when the browser closes
-            httpOnly: true, // when true, cookie is not accessible from javascript
-            // TODO: create a proper environment variable for this situation.
-            // TODO: replace all instances of process.env.NODE_ENV conditions with their own env vars.
-            secureProxy: process.env.NODE_ENV === 'production' // when true, cookie will only be sent over SSL. use key 'proxySecure' instead if you handle SSL not in your node process
-        }
-    })
-);
-
-app.use(
-    csrf({
-        cookie: false,
-        sessionKey: 'session'
-    })
-);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
@@ -110,9 +88,14 @@ app.use(
     express.static(path.join(__dirname, '/node_modules/govuk-frontend/govuk/all.js'))
 );
 
-app.use(async (req, res, next) => {
+async function keepAlive(req, res, next) {
     try {
-        if (!req.originalUrl.startsWith('/session') && req.session.questionnaireId) {
+        if (!req.originalUrl.startsWith('/session') && isQuestionnaireInstantiated(req.session)) {
+            const accountService = createAccountService(req.session);
+            const questionnaireService = createQuestionnaireService({
+                ownerId: accountService.getOwnerId()
+            });
+            const questionnaireId = getQuestionnaireIdInSession(req.session);
             const cookieExpiryService = createCookieService({
                 req,
                 res,
@@ -120,11 +103,11 @@ app.use(async (req, res, next) => {
             });
 
             if (cookieExpiryService.isSet('expires') && cookieExpiryService.isExpired()) {
-                req.session.destroy();
+                res.clearCookie('session');
                 cookieExpiryService.set('expires', '');
                 return next();
             }
-            const response = await qService.keepAlive(req.session.questionnaireId);
+            const response = await questionnaireService.keepAlive(questionnaireId);
             const sessionResource = response.body.data[0].attributes;
             cookieExpiryService.set({
                 alive: sessionResource.alive,
@@ -138,38 +121,76 @@ app.use(async (req, res, next) => {
     }
 
     return next();
-});
+}
 
-// Suppression necessary as 'return' is needed to call res.end() end prevent the redirect throwing an error.
-// eslint-disable-next-line consistent-return
-app.use(['/apply', '/download'], async (req, res, next) => {
-    if (!req.session.questionnaireId) {
-        // no: set it and redirect.
-        try {
-            const response = await qService.createQuestionnaire();
-            req.session.questionnaireId = response.body.data.attributes.id;
-            const initialSection = formHelper.removeSectionIdPrefix(
-                response.body.data.attributes.routes.initial
-            );
-            const baseUrl = req.baseUrl === '/download' ? '/apply' : req.baseUrl;
-            let redirectionUrl = `${baseUrl}/${initialSection}`;
-            // query param passed from Tempus launch page
-            if (req.query.isCica) {
-                redirectionUrl = `${redirectionUrl}?isCica=true`;
-            }
-            return res.redirect(redirectionUrl);
-        } catch (err) {
-            return res.status(404).render('404.njk');
+const oidcConfig = {
+    authRequired: false,
+    auth0Logout: false,
+    idpLogout: true,
+    baseURL: `${process.env.CW_URL}/account`,
+    clientID: process.env.CW_GOVUK_CLIENT_ID,
+    clientAuthMethod: 'private_key_jwt',
+    clientAssertionSigningKey: process.env.CW_GOVUK_PRIVATE_KEY,
+    idTokenSigningAlg: 'ES256',
+    issuerBaseURL: process.env.CW_GOVUK_ISSUER_URL,
+    secret: process.env.CW_COOKIE_SECRET,
+    session: {
+        name: 'session',
+        rolling: true,
+        // `CW_SESSION_DURATION` is represented in ms for consistency.
+        rollingDuration: process.env.CW_SESSION_DURATION / 1000,
+        cookie: {
+            transient: true,
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production'
         }
+    },
+    authorizationParams: {
+        response_type: 'code',
+        scope: 'openid'
+    },
+    routes: {
+        // alternative is described here:
+        // https://github.com/auth0/express-openid-connect/blob/master/EXAMPLES.md#3-route-customization
+        // seems somewhat convoluted for what we need the callback for.
+        // https://github.com/auth0/express-openid-connect/blob/master/examples/custom-routes.js
+        callback: '/signed-in',
+        login: false,
+        logout: '/sign-out',
+        postLogoutRedirect: '/signed-out'
+    },
+    afterCallback: async (req, res, session) => {
+        return {
+            ...session,
+            questionnaireId: req.session.questionnaireId,
+            ownerId: req.session.ownerId
+        };
     }
-    next(); // <-- important!
+};
+
+app.use(
+    csrf({
+        cookie: true,
+        sessionKey: 'session'
+    })
+);
+
+app.use((req, res, next) => {
+    res.set({
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        Pragma: 'no-cache',
+        Expires: '0',
+        'Surrogate-Control': 'no-store'
+    });
+    next();
 });
 
 app.use('/address-finder', addressFinderRouter);
-app.use('/download', downloadRouter);
-app.use('/apply', applicationRouter);
-app.use('/session', sessionRouter);
-app.use('/', indexRouter);
+app.use('/download', auth(oidcConfig), downloadRouter);
+app.use('/apply', auth(oidcConfig), keepAlive, applicationRouter);
+app.use('/session', auth(oidcConfig), sessionRouter);
+app.use('/account', auth(oidcConfig), accountRouter);
+app.use('/', auth(oidcConfig), indexRouter);
 
 app.use((err, req, res, next) => {
     if (err.name === 'CRNNotRetrieved') {
