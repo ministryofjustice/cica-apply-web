e8decc61bad4f8b21ce935e2229179d236ecf29f
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2023-07-14 11:29:54 +0100

    Add test coverage for `account service`

---
+++ account/account-service.test.js
@@ -0,0 +1,57 @@
+/* eslint-disable global-require */
+
+'use strict';
+
+let createAccountService;
+
+const OWNER_ID = 'urn:fdc:gov.uk:2022:dGhpc2lzYW5vd25lcmlkdGhpc2lzYW5vd25lcmlkdGh';
+const SESSION_WITH_OWNER = {
+    ownerId: OWNER_ID
+};
+const SESSION_WITHOUT_OWNER = {};
+
+const REGEX_OWNER_ID_ANONYMOUS = /^urn:uuid:[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+const REGEX_OWNER_ID_ASSIGNED = /^(urn:fdc:gov\.uk:2022:[A-Za-z0-9+/=-_]{1,44})$/i;
+
+beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+    createAccountService = require('./account-service');
+});
+
+describe('Account service', () => {
+    describe('session not populated with ownerId', () => {
+        describe('getOwnerId', () => {
+            it('Should generate a URN with GUID', () => {
+                const accountService = createAccountService(SESSION_WITHOUT_OWNER);
+                const ownerId = accountService.getOwnerId();
+                expect(ownerId).toMatch(REGEX_OWNER_ID_ANONYMOUS);
+            });
+        });
+        describe('setOwnerId', () => {
+            it('Should set an ownerId', () => {
+                const accountService = createAccountService(SESSION_WITHOUT_OWNER);
+                accountService.setOwnerId(OWNER_ID);
+                const ownerId = accountService.getOwnerId();
+                expect(ownerId).toMatch(REGEX_OWNER_ID_ASSIGNED);
+            });
+        });
+    });
+    describe('session populated with ownerId', () => {
+        describe('getOwnerId', () => {
+            it('Should retrieve a URN', () => {
+                const accountService = createAccountService(SESSION_WITH_OWNER);
+                const ownerId = accountService.getOwnerId();
+                expect(ownerId).toMatch(REGEX_OWNER_ID_ASSIGNED);
+            });
+        });
+        describe('setOwnerId', () => {
+            it('Should set an ownerId', () => {
+                const accountService = createAccountService(SESSION_WITH_OWNER);
+                accountService.setOwnerId('mynewownerid');
+                const ownerId = accountService.getOwnerId();
+                expect(ownerId).toMatch('mynewownerid');
+            });
+        });
+    });
+});
