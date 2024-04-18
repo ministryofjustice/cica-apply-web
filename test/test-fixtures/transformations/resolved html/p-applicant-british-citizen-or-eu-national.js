9a91ab51b9507c525c637f0722c0a085fb0fa7c0
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2024-04-18 08:21:35 +0100

    Remove references to IE8

--- test/test-fixtures/transformations/resolved html/p-applicant-british-citizen-or-eu-national.js
+++ test/test-fixtures/transformations/resolved html/p-applicant-british-citizen-or-eu-national.js
@@ -32,89 +32,8 @@ const html = `<!DOCTYPE html>
           'user_id': ''
         });
     </script>
-
-      <a href="#main-content" class="govuk-skip-link">Skip to main content</a>
-      <div class="cookie-banner govuk-width-container" id="cookie-banner">
-      <div class="govuk-grid-row">
-          <div class=" govuk-grid-column-two-thirds">
-              <div class="cookie-banner__message">
-                  <h2 class="govuk-heading-m">Tell us whether you accept cookies</h2>
-                  <p class="govuk-body">This service uses cookies that are essential for the site to work. We also use non-essential cookies to help us improve your experience.</p>
-                  <p class="govuk-body">Do you accept these non-essential cookies?</p>
-              </div>
-              <div class="cookie-banner__buttons">
-                  <div class="cookie-banner__button cookie-banner__button-accept govuk-grid-column-full govuk-grid-column-one-half-from-desktop govuk-!-padding-left-0">
-                      <a href="/cookies" id="cookie-banner-accept-all" class="govuk-button button--inline" role="button">Accept all cookies</a>
-                  </div>
-                  <div class="cookie-banner__button govuk-grid-column-full govuk-grid-column-one-half-from-desktop govuk-!-padding-left-0">
-                      <a href="/cookies" id="cookie-banner-set-preferences" class="govuk-button govuk-button--secondary button--inline" role="button">Set cookie preferences</a>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
-
-
-
-  <header class="govuk-header " role="banner" data-module="govuk-header">
-  <div class="govuk-header__container govuk-width-container">
-    <div class="govuk-header__logo">
-      <a href="https://www.gov.uk" class="govuk-header__link govuk-header__link--homepage">
-        <span class="govuk-header__logotype">
-          
-<!--[if gt IE 8]><!-->
-<svg
-  aria-hidden="true"
-  focusable="false"
-  class="govuk-header__logotype-crown"
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 32 30"
-  height="30"
-  width="32"
->
-  <path
-    fill="currentColor" fill-rule="evenodd"
-    d="M22.6 10.4c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4m-5.9 6.7c-.9.4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4m10.8-3.7c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s0 2-1 2.4m3.3 4.8c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4M17 4.7l2.3 1.2V2.5l-2.3.7-.2-.2.9-3h-3.4l.9 3-.2.2c-.1.1-2.3-.7-2.3-.7v3.4L15 4.7c.1.1.1.2.2.2l-1.3 4c-.1.2-.1.4-.1.6 0 1.1.8 2 1.9 2.2h.7c1-.2 1.9-1.1 1.9-2.1 0-.2 0-.4-.1-.6l-1.3-4c-.1-.2 0-.2.1-.3m-7.6 5.7c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s0 2 1 2.4m-5 3c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s.1 2 1 2.4m-3.2 4.8c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s0 2 1 2.4m14.8 11c4.4 0 8.6.3 12.3.8 1.1-4.5 2.4-7 3.7-8.8l-2.5-.9c.2 1.3.3 1.9 0 2.7-.4-.4-.8-1.1-1.1-2.3l-1.2 4c.7-.5 1.3-.8 2-.9-1.1 2.5-2.6 3.1-3.5 3-1.1-.2-1.7-1.2-1.5-2.1.3-1.2 1.5-1.5 2.1-.1 1.1-2.3-.8-3-2-2.3 1.9-1.9 2.1-3.5.6-5.6-2.1 1.6-2.1 3.2-1.2 5.5-1.2-1.4-3.2-.6-2.5 1.6.9-1.4 2.1-.5 1.9.8-.2 1.1-1.7 2.1-3.5 1.9-2.7-.2-2.9-2.1-2.9-3.6.7-.1 1.9.5 2.9 1.9l.4-4.3c-1.1 1.1-2.1 1.4-3.2 1.4.4-1.2 2.1-3 2.1-3h-5.4s1.7 1.9 2.1 3c-1.1 0-2.1-.2-3.2-1.4l.4 4.3c1-1.4 2.2-2 2.9-1.9-.1 1.5-.2 3.4-2.9 3.6-1.9.2-3.4-.8-3.5-1.9-.2-1.3 1-2.2 1.9-.8.7-2.3-1.2-3-2.5-1.6.9-2.2.9-3.9-1.2-5.5-1.5 2-1.3 3.7.6 5.6-1.2-.7-3.1 0-2 2.3.6-1.4 1.8-1.1 2.1.1.2.9-.3 1.9-1.5 2.1-.9.2-2.4-.5-3.5-3 .6 0 1.2.3 2 .9l-1.2-4c-.3 1.1-.7 1.9-1.1 2.3-.3-.8-.2-1.4 0-2.7l-2.9.9C1.3 23 2.6 25.5 3.7 30c3.7-.5 7.9-.8 12.3-.8"></path>
-</svg>
-<!--<![endif]-->
-<!--[if IE 8]>
-<img src="/assets/images/govuk-logotype-tudor-crown.png" class="govuk-header__logotype-crown-fallback-image" width="32" height="30" alt="">
-<![endif]-->
-
-          <span class="govuk-header__logotype-text">
-            GOV.UK
-          </span>
-        </span>
-        
-      </a>
-    </div>
-    
-    <div class="govuk-header__content">
-    
-    <a href="https://www.gov.uk/claim-compensation-criminal-injury/make-claim" class="govuk-header__link govuk-header__link--service-name">
-      Claim criminal injuries compensation
-    </a>
-    
-    
-    </div>
-    
-  </div>
-</header>
-
-
-<div class="govuk-width-container" role="navigation">
-    <div class="govuk-grid-column-two-thirds">
-        <a href="/apply/previous/applicant-british-citizen-or-eu-national" class="govuk-back-link">Back</a>
-    </div>
-</div>
-
-
-
-      <div class="govuk-width-container">
-
-
-        <main class="govuk-main-wrapper " id="main-content" role="main">
-
+      <a href="#main-content" class="govuk-skip-link" data-module="govuk-skip-link">Skip to main content</a>
+    <div class="cookie-banner govuk-width-container" id="cookie-banner">
     <div class="govuk-grid-row">
         <div class=" govuk-grid-column-two-thirds">
             <div class="cookie-banner__message">
@@ -159,11 +78,11 @@ const html = `<!DOCTYPE html>
     </div>
   </div>
 </header>
-  <div class="govuk-width-container" role="navigation">
+    <div class="govuk-width-container" role="navigation">
                 <div class="govuk-grid-column-two-thirds">
                         <a href="/apply/previous/applicant-british-citizen-or-eu-national" class="govuk-back-link">Back</a>
                 </div>
-  </div>
+    </div>
       <div class="govuk-width-container">
         <main class="govuk-main-wrapper" id="main-content" role="main">
     <div class="govuk-grid-row">
@@ -203,7 +122,8 @@ const html = `<!DOCTYPE html>
       </div>
     <div class="govuk-width-container">
         <hr class="govuk-section-break govuk-section-break--m govuk-section-break--visible">
-        <p class="govuk-body-s"><a class="govuk-link" href="https://www.surveymonkey.co.uk/r/YourFeedbackPB" target="_blank">Tell us your feedback (opens in new tab)</a> to help us to improve our service.</p>
+        <p class="govuk-body-s">
+            <a class="govuk-link" href="https://www.surveymonkey.co.uk/r/YourFeedbackPB" target="_blank">Tell us your feedback (opens in new tab)</a> to help us to improve our service.</p>
     </div>
     <footer class="govuk-footer" role="contentinfo">
   <div class="govuk-width-container">
@@ -278,7 +198,6 @@ const html = `<!DOCTYPE html>
             tabindex="0"
         >
             <div class="govuk-modal__header">
-                <!--[if gt IE 8]><!-->
                 <svg
                     aria-hidden="true"
                     focusable="false"
@@ -292,10 +211,6 @@ const html = `<!DOCTYPE html>
                     fill="currentColor" fill-rule="evenodd"
                     d="M22.6 10.4c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4m-5.9 6.7c-.9.4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4m10.8-3.7c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s0 2-1 2.4m3.3 4.8c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4M17 4.7l2.3 1.2V2.5l-2.3.7-.2-.2.9-3h-3.4l.9 3-.2.2c-.1.1-2.3-.7-2.3-.7v3.4L15 4.7c.1.1.1.2.2.2l-1.3 4c-.1.2-.1.4-.1.6 0 1.1.8 2 1.9 2.2h.7c1-.2 1.9-1.1 1.9-2.1 0-.2 0-.4-.1-.6l-1.3-4c-.1-.2 0-.2.1-.3m-7.6 5.7c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s0 2 1 2.4m-5 3c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s.1 2 1 2.4m-3.2 4.8c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s0 2 1 2.4m14.8 11c4.4 0 8.6.3 12.3.8 1.1-4.5 2.4-7 3.7-8.8l-2.5-.9c.2 1.3.3 1.9 0 2.7-.4-.4-.8-1.1-1.1-2.3l-1.2 4c.7-.5 1.3-.8 2-.9-1.1 2.5-2.6 3.1-3.5 3-1.1-.2-1.7-1.2-1.5-2.1.3-1.2 1.5-1.5 2.1-.1 1.1-2.3-.8-3-2-2.3 1.9-1.9 2.1-3.5.6-5.6-2.1 1.6-2.1 3.2-1.2 5.5-1.2-1.4-3.2-.6-2.5 1.6.9-1.4 2.1-.5 1.9.8-.2 1.1-1.7 2.1-3.5 1.9-2.7-.2-2.9-2.1-2.9-3.6.7-.1 1.9.5 2.9 1.9l.4-4.3c-1.1 1.1-2.1 1.4-3.2 1.4.4-1.2 2.1-3 2.1-3h-5.4s1.7 1.9 2.1 3c-1.1 0-2.1-.2-3.2-1.4l.4 4.3c1-1.4 2.2-2 2.9-1.9-.1 1.5-.2 3.4-2.9 3.6-1.9.2-3.4-.8-3.5-1.9-.2-1.3 1-2.2 1.9-.8.7-2.3-1.2-3-2.5-1.6.9-2.2.9-3.9-1.2-5.5-1.5 2-1.3 3.7.6 5.6-1.2-.7-3.1 0-2 2.3.6-1.4 1.8-1.1 2.1.1.2.9-.3 1.9-1.5 2.1-.9.2-2.4-.5-3.5-3 .6 0 1.2.3 2 .9l-1.2-4c-.3 1.1-.7 1.9-1.1 2.3-.3-.8-.2-1.4 0-2.7l-2.9.9C1.3 23 2.6 25.5 3.7 30c3.7-.5 7.9-.8 12.3-.8"></path>
                 </svg>
-                <!--<![endif]-->
-                <!--[if IE 8]>
-                    <img src="/assets/images/govuk-logotype-tudor-crown.png" class="govuk-header__logotype-crown-fallback-image" width="32" height="30" alt="">
-                <![endif]-->
             </div>
             <div class="govuk-modal__main">
                     <span class="govuk-modal__heading govuk-heading-l" id="session-timing-out-title">
@@ -325,7 +240,6 @@ const html = `<!DOCTYPE html>
             tabindex="0"
         >
             <div class="govuk-modal__header">
-                <!--[if gt IE 8]><!-->
                 <svg
                     aria-hidden="true"
                     focusable="false"
@@ -339,10 +253,6 @@ const html = `<!DOCTYPE html>
                     fill="currentColor" fill-rule="evenodd"
                     d="M22.6 10.4c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4m-5.9 6.7c-.9.4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4m10.8-3.7c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s0 2-1 2.4m3.3 4.8c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4M17 4.7l2.3 1.2V2.5l-2.3.7-.2-.2.9-3h-3.4l.9 3-.2.2c-.1.1-2.3-.7-2.3-.7v3.4L15 4.7c.1.1.1.2.2.2l-1.3 4c-.1.2-.1.4-.1.6 0 1.1.8 2 1.9 2.2h.7c1-.2 1.9-1.1 1.9-2.1 0-.2 0-.4-.1-.6l-1.3-4c-.1-.2 0-.2.1-.3m-7.6 5.7c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s0 2 1 2.4m-5 3c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s.1 2 1 2.4m-3.2 4.8c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s0 2 1 2.4m14.8 11c4.4 0 8.6.3 12.3.8 1.1-4.5 2.4-7 3.7-8.8l-2.5-.9c.2 1.3.3 1.9 0 2.7-.4-.4-.8-1.1-1.1-2.3l-1.2 4c.7-.5 1.3-.8 2-.9-1.1 2.5-2.6 3.1-3.5 3-1.1-.2-1.7-1.2-1.5-2.1.3-1.2 1.5-1.5 2.1-.1 1.1-2.3-.8-3-2-2.3 1.9-1.9 2.1-3.5.6-5.6-2.1 1.6-2.1 3.2-1.2 5.5-1.2-1.4-3.2-.6-2.5 1.6.9-1.4 2.1-.5 1.9.8-.2 1.1-1.7 2.1-3.5 1.9-2.7-.2-2.9-2.1-2.9-3.6.7-.1 1.9.5 2.9 1.9l.4-4.3c-1.1 1.1-2.1 1.4-3.2 1.4.4-1.2 2.1-3 2.1-3h-5.4s1.7 1.9 2.1 3c-1.1 0-2.1-.2-3.2-1.4l.4 4.3c1-1.4 2.2-2 2.9-1.9-.1 1.5-.2 3.4-2.9 3.6-1.9.2-3.4-.8-3.5-1.9-.2-1.3 1-2.2 1.9-.8.7-2.3-1.2-3-2.5-1.6.9-2.2.9-3.9-1.2-5.5-1.5 2-1.3 3.7.6 5.6-1.2-.7-3.1 0-2 2.3.6-1.4 1.8-1.1 2.1.1.2.9-.3 1.9-1.5 2.1-.9.2-2.4-.5-3.5-3 .6 0 1.2.3 2 .9l-1.2-4c-.3 1.1-.7 1.9-1.1 2.3-.3-.8-.2-1.4 0-2.7l-2.9.9C1.3 23 2.6 25.5 3.7 30c3.7-.5 7.9-.8 12.3-.8"></path>
                 </svg>
-                <!--<![endif]-->
-                <!--[if IE 8]>
-                    <img src="/assets/images/govuk-logotype-tudor-crown.png" class="govuk-header__logotype-crown-fallback-image" width="32" height="30" alt="">
-                <![endif]-->
             </div>
             <div class="govuk-modal__main">
                     <span class="govuk-modal__heading govuk-heading-l" id="session-ended-title">
@@ -372,7 +282,6 @@ const html = `<!DOCTYPE html>
             tabindex="0"
         >
             <div class="govuk-modal__header">
-                <!--[if gt IE 8]><!-->
                 <svg
                     aria-hidden="true"
                     focusable="false"
@@ -386,10 +295,6 @@ const html = `<!DOCTYPE html>
                     fill="currentColor" fill-rule="evenodd"
                     d="M22.6 10.4c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4m-5.9 6.7c-.9.4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4m10.8-3.7c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s0 2-1 2.4m3.3 4.8c-1 .4-2-.1-2.4-1-.4-.9.1-2 1-2.4.9-.4 2 .1 2.4 1s-.1 2-1 2.4M17 4.7l2.3 1.2V2.5l-2.3.7-.2-.2.9-3h-3.4l.9 3-.2.2c-.1.1-2.3-.7-2.3-.7v3.4L15 4.7c.1.1.1.2.2.2l-1.3 4c-.1.2-.1.4-.1.6 0 1.1.8 2 1.9 2.2h.7c1-.2 1.9-1.1 1.9-2.1 0-.2 0-.4-.1-.6l-1.3-4c-.1-.2 0-.2.1-.3m-7.6 5.7c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s0 2 1 2.4m-5 3c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s.1 2 1 2.4m-3.2 4.8c.9.4 2-.1 2.4-1 .4-.9-.1-2-1-2.4-.9-.4-2 .1-2.4 1s0 2 1 2.4m14.8 11c4.4 0 8.6.3 12.3.8 1.1-4.5 2.4-7 3.7-8.8l-2.5-.9c.2 1.3.3 1.9 0 2.7-.4-.4-.8-1.1-1.1-2.3l-1.2 4c.7-.5 1.3-.8 2-.9-1.1 2.5-2.6 3.1-3.5 3-1.1-.2-1.7-1.2-1.5-2.1.3-1.2 1.5-1.5 2.1-.1 1.1-2.3-.8-3-2-2.3 1.9-1.9 2.1-3.5.6-5.6-2.1 1.6-2.1 3.2-1.2 5.5-1.2-1.4-3.2-.6-2.5 1.6.9-1.4 2.1-.5 1.9.8-.2 1.1-1.7 2.1-3.5 1.9-2.7-.2-2.9-2.1-2.9-3.6.7-.1 1.9.5 2.9 1.9l.4-4.3c-1.1 1.1-2.1 1.4-3.2 1.4.4-1.2 2.1-3 2.1-3h-5.4s1.7 1.9 2.1 3c-1.1 0-2.1-.2-3.2-1.4l.4 4.3c1-1.4 2.2-2 2.9-1.9-.1 1.5-.2 3.4-2.9 3.6-1.9.2-3.4-.8-3.5-1.9-.2-1.3 1-2.2 1.9-.8.7-2.3-1.2-3-2.5-1.6.9-2.2.9-3.9-1.2-5.5-1.5 2-1.3 3.7.6 5.6-1.2-.7-3.1 0-2 2.3.6-1.4 1.8-1.1 2.1.1.2.9-.3 1.9-1.5 2.1-.9.2-2.4-.5-3.5-3 .6 0 1.2.3 2 .9l-1.2-4c-.3 1.1-.7 1.9-1.1 2.3-.3-.8-.2-1.4 0-2.7l-2.9.9C1.3 23 2.6 25.5 3.7 30c3.7-.5 7.9-.8 12.3-.8"></path>
                 </svg>
-                <!--<![endif]-->
-                <!--[if IE 8]>
-                    <img src="/assets/images/govuk-logotype-tudor-crown.png" class="govuk-header__logotype-crown-fallback-image" width="32" height="30" alt="">
-                <![endif]-->
 <button type="button" class="govuk-button govuk-modal__close" data-module="govuk-button"
    aria-label="Close modal">
   ×
@@ -402,7 +307,7 @@ const html = `<!DOCTYPE html>
                     <div class="govuk-modal__content" id="session-resume-error-content">
     <p class="govuk-body">We're unable to resume this application. Unless you were signed in and saved your progress, you'll have to start your application again.</p>
                     </div>
-<a href="/apply" role="button" draggable="false" class="govuk-button ga-event--click" data-module="govuk-button" 
+<a href="/apply" role="button" draggable="false" class="govuk-button ga-event--click" data-module="govuk-button"
    data-tracking-category="modal-button" data-tracking-label="Start again">
   Continue
 </a>
@@ -411,19 +316,21 @@ const html = `<!DOCTYPE html>
     </div>
     <div class="govuk-modal__overlay"></div>
 </div>
-
-<script nonce="somenonce">
-window.CICA = {
-  SERVICE_URL: 'http://www.b44e2eaa-baf5-47aa-8ac9-5d23ee2a7297.gov.uk',
-  ANALYTICS_TRACKING_ID: '',
-  CICA_ANALYTICS_ID: ''
-};
-</script>
-<script nonce="somenonce" src="/govuk-frontend/all.js"></script>
-  <script nonce="somenonce">window.GOVUKFrontend.initAll()</script>
-  <script nonce="somenonce" src="https://code.jquery.com/jquery-3.6.3.min.js "integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="crossorigin="anonymous"></script>
-  <script nonce="somenonce" src="/dist/js/autocomplete.min.js"></script>
-  <script nonce="somenonce" src="/dist/js/bundle.js?1.2.3"></script>
+    <script nonce="somenonce">
+        window.CICA = {
+            SERVICE_URL: 'http://www.b44e2eaa-baf5-47aa-8ac9-5d23ee2a7297.gov.uk',
+            ANALYTICS_TRACKING_ID: '',
+            CICA_ANALYTICS_ID: ''
+        };
+    </script>
+    <script type="module" nonce="somenonce" src="/govuk-frontend/all.js?1.2.3"></script>
+    <script type="module" nonce="somenonce">
+        import {initAll} from '/govuk-frontend/all.js?1.2.3'
+        initAll();
+    </script>
+    <script nonce="somenonce" src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script nonce="somenonce" src="/dist/js/autocomplete.min.js?1.2.3"></script>
+    <script nonce="somenonce" src="/dist/js/bundle.js?1.2.3"></script>
   </body>
 </html>
 `;
