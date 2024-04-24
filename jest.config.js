0d4a4751f3bf8e8f7a5c59681024875b41d9d61f
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2024-04-24 12:42:31 +0100

    WIP

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ process.env.CW_INTERNAL_IP = '1.0.0.0';
 const config = {
     testEnvironment: 'node',
     setupFilesAfterEnv: ['./jest.setup.js'],
-    coveragePathIgnorePatterns: ['./account/routes.js'],
+    coveragePathIgnorePatterns: ['./account/routes.js', './questionnaire/task-list/routes.js'],
     coverageThreshold: {
         './**/*.js': {
             branches: 60,
