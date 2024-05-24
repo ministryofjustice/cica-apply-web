967c24f3a9417d0bfdbbfecbc4cbc3eb7d024ff2
tjbburton <Thomas.Burton@CICA.gov.uk>
2024-05-24 11:25:44 +0100

    Add 404 errors

--- questionnaire/actions/actionRoutes.js
+++ questionnaire/actions/actionRoutes.js
@@ -13,13 +13,17 @@ const getOwnerOrigin = require('../utils/getOwnerOrigin');
 
 const router = express.Router();
 
-// router.get('/:action', (req, res) => {
-//     try {
-//         return res.redirect(`/${req.params.action}/start`);
-//     } catch (err) {
-//         return res.status(err.statusCode || 404).render('404.njk');
-//     }
-// });
+router.get('/', (req, res) => {
+    return res.status(404).render('404.njk');
+});
+
+router.get('/:action', (req, res) => {
+    try {
+        return res.redirect(`/${req.params.action}/start`);
+    } catch (err) {
+        return res.status(err.statusCode || 404).render('404.njk');
+    }
+});
 
 // TODO: delete this route before release; not needed, just using for testing
 router.route('/:action/start').get(async (req, res) => {
@@ -193,6 +197,7 @@ router
     })
     .post(async (req, res, next) => {
         try {
+            console.log(req.baseUrl);
             const accountService = createAccountService(req.session);
             const isAuthenticated = accountService.isAuthenticated(req);
             const questionnaireService = createQuestionnaireService({
