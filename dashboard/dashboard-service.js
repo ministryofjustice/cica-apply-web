03d2d75675441861404bf08f442caffec9e2f23a
tjbburton <Thomas.Burton@CICA.gov.uk>
2024-10-11 11:18:29 +0100

    Update dashboard content and hide empty tables

--- dashboard/dashboard-service.js
+++ dashboard/dashboard-service.js
@@ -10,28 +10,31 @@ function createDashboardService(ownerId) {
             return {
                 questionnaireId: metadatum.attributes['questionnaire-id'],
                 expires: metadatum.attributes.expires,
-                externalId: metadatum.attributes?.['external-id']
+                externalId: metadatum.attributes?.['external-id'],
+                type: metadatum.attributes['template-type']
             };
         });
     }
 
-    async function getTemplateData() {
+    async function getApplicationData() {
         const dataset = [];
         // eslint-disable-next-line no-restricted-syntax
         for await (const flatMetadata of await getFlatQuestionnaireMetadataByUserId()) {
-            const answers = await questionnaireService.getSectionAnswers(
-                flatMetadata.questionnaireId,
-                'p-applicant-enter-your-name'
-            );
-            dataset.push({
-                ...flatMetadata,
-                answers: {
-                    'q-applicant-title': '',
-                    'q-applicant-first-name': '',
-                    'q-applicant-last-name': '',
-                    ...answers.body?.data?.attributes
-                }
-            });
+            if (flatMetadata.type === 'apply-for-compensation') {
+                const answers = await questionnaireService.getSectionAnswers(
+                    flatMetadata.questionnaireId,
+                    'p-applicant-enter-your-name'
+                );
+                dataset.push({
+                    ...flatMetadata,
+                    answers: {
+                        'q-applicant-title': '',
+                        'q-applicant-first-name': '',
+                        'q-applicant-last-name': '',
+                        ...answers.body?.data?.attributes
+                    }
+                });
+            }
         }
         const templateData = dataset.reduce((acc, questionnaireData) => {
             const resumeLink = questionnaireData.externalId
@@ -63,8 +66,55 @@ function createDashboardService(ownerId) {
         return templateData;
     }
 
+    async function getActionData() {
+        const dataset = [];
+        // eslint-disable-next-line no-restricted-syntax
+        for await (const flatMetadata of await getFlatQuestionnaireMetadataByUserId()) {
+            if (flatMetadata.type !== 'apply-for-compensation') {
+                const submissionData = await questionnaireService.getSubmission(
+                    flatMetadata.questionnaireId
+                );
+                dataset.push({
+                    ...flatMetadata,
+                    data: {
+                        caseReferenceNumber: '',
+                        ...submissionData.body?.data?.attributes
+                    }
+                });
+            }
+        }
+        const templateData = dataset.reduce((acc, questionnaireData) => {
+            const resumeLink = questionnaireData.analyticsId
+                ? `/apply/resume/${questionnaireData.questionnaireId}?a_id=${questionnaireData.analyticsId}`
+                : `/apply/resume/${questionnaireData.questionnaireId}`;
+            acc.push([
+                {
+                    text: `${questionnaireData.data.caseReferenceNumber}`,
+                    classes: 'govuk-table__cell__overflow'
+                },
+                {
+                    text: new Date(questionnaireData.expires).toLocaleDateString('en-GB', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                        timeZone: 'Europe/London'
+                    }),
+                    attributes: {
+                        'data-sort-value': new Date(questionnaireData.expires).getTime()
+                    }
+                },
+                {
+                    html: `<a href="${resumeLink}" class="govuk-link">View decision<span class='govuk-visually-hidden'> View decision for case ${questionnaireData.data.caseReferenceNumber}</span></a>`
+                }
+            ]);
+            return acc;
+        }, []);
+
+        return templateData;
+    }
     return Object.freeze({
-        getTemplateData
+        getApplicationData,
+        getActionData
     });
 }
 
